cfc1afa9784a5586d6d00aa504a80b219af49db2
fpreston1 <42384725+fpreston1@users.noreply.github.com>
2018-08-14 13:36:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -49,28 +49,7 @@ bot.on("message", function(message) {
     message.channel.sendEmbed(embed);
         break;
 
-	case "report":
-	let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-	if(!rUser) return message.channel.send("Couldn't find the user");
-	let reason = args.join(" ").slice(22);
-
-	let reportEmbed = new Discord.RichEmbed()
-	.setDescription("Reports")
-	.setColor("6812512")
-	.addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-	.addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-	.addField("Channel", message.channel)
-	.addField("Time", message.createdAt)
-	.addField("Reason", reason);
-
-	let reportschannel = message.guild.channels.find(`name`, "reports");
-	if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
 	
-
-		message.delete().catch(O_o=>{});
-		reportschannel.send(reportEmbed);
-
-	break;
 
 	case "invite":
 	let memeEmbed = new Discord.RichEmbed()
