b90e376870c967ea03113c5cb1993707e9a83d9f
fpreston1 <42384725+fpreston1@users.noreply.github.com>
2018-08-18 22:42:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ bot.on("message", async message => {
 	.setThumbnail(nficon)
 	.addField("Alert:", "- A scrim match is starting very soon! @everyone")
 	.addField("Instructions:", "- We will countdown from 3 sec and you will ready up on go.")
-	.setFooter(`🔴 Match lead by ${message.author.username} on ${message.createdAt}`)
+	.setFooter(`🔴 Match lead on ${message.createdAt}`)
 	.setColor(6812512);
 		
 	
