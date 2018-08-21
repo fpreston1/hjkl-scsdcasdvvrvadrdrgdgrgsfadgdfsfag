03954714d757fc425037d4babb197074011840f4
fpreston1 <42384725+fpreston1@users.noreply.github.com>
2018-08-21 16:20:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -254,14 +254,14 @@ bot.on("message", async message => {
 	let last3chan = message.guild.channels.find(`name`, "scrim-last3");
 	let sayEmbed = new Discord.RichEmbed()
 	.setTitle("Current Servers:")
-	.addField(`${args[0] || `\u200b`}`, `*${args[1] || `\u200b`}*`, true)
-	.addField(`${args[2] || `\u200b`}`, `*${args[3] || `\u200b`}*`, true)
-	.addField(`${args[4] || `\u200b`}`, `*${args[5] || `\u200b`}*`, true)
-	.addField(`${args[6] || `\u200b`}`, `*${args[7] || `\u200b`}*`, true)
-	.addField(`${args[8] || `\u200b`}`, `*${args[9] || `\u200b`}*`, true)
-	.addField(`${args[10] || `\u200b`}`, `*${args[11] || `\u200b`}*`, true)
-	.addField(`${args[12] || `\u200b`}`, `*${args[13] || `\u200b`}*`, true)
-	.addField(`${args[14] || `\u200b`}`, `*${args[15] || `\u200b`}*`, true)
+	.addField(`${args[0] || `\u200b`}`, `*${args[1] || `\u200b`}*`)
+	.addField(`${args[2] || `\u200b`}`, `*${args[3] || `\u200b`}*`)
+	.addField(`${args[4] || `\u200b`}`, `*${args[5] || `\u200b`}*`)
+	.addField(`${args[6] || `\u200b`}`, `*${args[7] || `\u200b`}*`)
+	.addField(`${args[8] || `\u200b`}`, `*${args[9] || `\u200b`}*`)
+	.addField(`${args[10] || `\u200b`}`, `*${args[11] || `\u200b`}*`)
+	.addField(`${args[12] || `\u200b`}`, `*${args[13] || `\u200b`}*`)
+	.addField(`${args[14] || `\u200b`}`, `*${args[15] || `\u200b`}*`)
 	.setFooter("Scrims hosted by Small Scrims Discord")
 	.setColor(13328335);
 
