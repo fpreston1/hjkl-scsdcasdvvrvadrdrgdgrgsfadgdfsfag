1dfe590e94e2efd4d1551724be2cfd1f1c63bc09
fpreston1 <42384725+fpreston1@users.noreply.github.com>
2018-08-22 14:14:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,14 +23,24 @@ bot.on("ready", async () => {
 //});
 
 bot.on('guildMemberAdd', member => {
-	member.guild.channels.find('name', 'choose-region').sendMessage(member.toString() + " Please set region!").then(msg => msg.delete(1000);
-	member.guild.channels.find('name', 'choose-platform').sendMessage(member.toString() + " Please set platform!").then(msg => msg.delete(1000);
-
-
-	var role = member.guild.roles.find('name', 'Starter');
+		let regionChannel = member.guild.channels.find('name', 'choose-region');
+		let platChannel = member.guild.channels.find('name', 'choose-platform');
+		var role = member.guild.roles.find('name', 'Starter');
 
 	
 	member.addRole(role);
+	
+	member.guild.channels.find('name', 'choose-region').sendMessage(member.toString() + " Set region!");
+	const regionTime = ms => new Promise(res => setTimeout(res, ms))
+	await regionTime(1000);
+	regionChannel.bulkDelete(1);
+	member.guild.channels.find('name', 'choose-platform').sendMessage(member.toString() + " Set platform!");
+	await regionTime(1000);
+	platChannel.bulkDelete(1);
+	
+
+	
+
 });
 
 
