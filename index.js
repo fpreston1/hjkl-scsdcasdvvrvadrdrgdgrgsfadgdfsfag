207ecd4577dad8a4024550458ef799f9cb4884d1
fpreston1 <42384725+fpreston1@users.noreply.github.com>
2018-08-23 20:41:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -401,20 +401,46 @@ bot.on("message", async message => {
 	function Play(connection, message) {
 	const YTDL = require("ytdl-core");
 	var servers = {};
-
-	var server = servers[message.guild.id];
-	
-	server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-	
-	//server.queue.shift();
-	
-	server.dispatcher.on("end", function() {
+		var server = servers[message.guild.id];
+		server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+		server.queue.shift();
+		server.dispatcher.on("end", function(){
+		if(server.queue[0]){
+		Play(connection,message);
+		}else{
+		connection.disconnect();
+		}
+		});
 		
-	 connection.disconnect();
+	}
+	
+	
+	if(cmd === `${prefix}pp`){
+	var servers = {};
+		if(message.member.voiceChannel)
+		{
+		if(!message.guild.voiceConnection)
+		{
+			if(!servers[message.guild.id]){
+				
+				servers[message.guild.id] = {queue: []}
+			   
+			   }
+			
+			message.member.voiceChannel.join()
+			.then(connection => {
+				var server = servers[message.guild.id];
+				message.reply("Joined!!").then(msg => msg.delete(1000));
+				server.queue.push(args[0]);
+				Play(connection, message);
+			})
+		}
+	}else{
+	message.reply("You must be in a voice channel!").then(msg => msg.delete(1000));
+	}
 		
-
-	})
-}
+	return;
+	}
 
 
 
