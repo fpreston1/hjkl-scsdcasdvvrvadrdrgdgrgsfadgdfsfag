dff1a5427e01b239f4e96fc41d0fd60fd3fd89f6
fpreston1 <42384725+fpreston1@users.noreply.github.com>
2018-08-24 18:42:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -431,38 +431,38 @@ bot.on("message", async message => {
 		
 	
 		
-	const yeetTim = ms => new Promise(res => setTimeout(res, ms))
-	await yeetTim(2000);
-		var servers = {};
-		if(message.member.voiceChannel)
-		{
-		if(!message.guild.voiceConnection)
-		{
-			if(!servers[message.guild.id]){
-				
-				servers[message.guild.id] = {queue: []}
-			   
-			   }
-			
-			message.member.voiceChannel.join()
-			.then(connection => {
-				var server = servers[message.guild.id];
-				message.reply("Joined!!").then(msg => msg.delete(1000));
-				server.queue.push(args[0] || "https://www.youtube.com/watch?v=erKU_Ro-poM&feature=youtu.be");
-				Play(connection, message);
-			})
-		}
-	}else{
-	message.reply("You must be in a voice channel!").then(msg => msg.delete(1000));
-	}
+	//const yeetTim = ms => new Promise(res => setTimeout(res, ms))
+	//await yeetTim(2000);
+	//	var servers = {};
+	//	if(message.member.voiceChannel)
+	//	{
+	//	if(!message.guild.voiceConnection)
+	//	{
+	//		if(!servers[message.guild.id]){
+	//			
+	//			servers[message.guild.id] = {queue: []}
+	//		   
+	//		   }
+	//		
+	//		message.member.voiceChannel.join()
+	//		.then(connection => {
+	//			var server = servers[message.guild.id];
+	//			message.reply("Joined!!").then(msg => msg.delete(1000));
+	//			server.queue.push(args[0] || "https://www.youtube.com/watch?v=erKU_Ro-poM&feature=youtu.be");
+	//			Play(connection, message);
+	//		})
+	//	}
+	//}else{
+	//message.reply("You must be in a voice channel!").then(msg => msg.delete(1000));
+	//}
 		
-	await yeetTim(15000);
-	if(message.guild.voiceConnection){
-			message.guild.voiceConnection.disconnect();
-		}else{
-		message.reply("Cannot do that.").then(msg => msg.delete(2000));
-	}
-	
+//	await yeetTim(15000);
+//	if(message.guild.voiceConnection){
+//			message.guild.voiceConnection.disconnect();
+//		}else{
+//		message.reply("Cannot do that.").then(msg => msg.delete(2000));
+//	}
+//	
 
 	
 	let scrimlast3chan = message.guild.channels.find(`name`, "scrim-last3");
@@ -733,10 +733,9 @@ bot.on("message", async message => {
 	if(message.channel.id === "481865517393510402") return;
 	
 	
-	let args = args.join(" ");
 	let username = args[0] || message.author.username;
 	let platform = "pc";
-	let gamemode = "lifetime";
+	let gamemode = args[1] || "lifetime";
 		
 	if(!args[0]) return message.channel.send("Format !fn <solo,duo,squad,lifetime> <username>");
 	
