adffa6b93950a4ce7f8796447a3b81165202b6ff
fpreston1 <42384725+fpreston1@users.noreply.github.com>
2018-09-03 14:19:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -643,6 +643,14 @@ bot.on("message", async message => {
 	.addField("Please enter the last 3 digits of your server!", "When in-game you can find this in the top left corner of your screen.")
 	.setColor(6812512);
 	scrimlast3chan.send(startEmbed);
+	scrimlast3chan.overwritePermissions(message.guild.id, {
+	SEND_MESSAGES: true
+	})
+	message.delete().catch(O_o=>{});
+		
+
+		
+
 	await startTimeout(10000);
 	const allCodeRoles = message.guild.roles
 		.filter(r => (/^\w{3}$/).test(r.name))
@@ -669,15 +677,6 @@ bot.on("message", async message => {
 			})
 			
 		}
-	scrimlast3chan.overwritePermissions(message.guild.id, {
-	SEND_MESSAGES: true
-	})
-	message.delete().catch(O_o=>{});
-		
-
-		
-
-	await startTimeout(15000);
 	let nextgameEmbed = new Discord.RichEmbed()
 	.setTitle("**Next snipe in approx...**")
 	.setDescription("*25 Minutes*")
