fd31041af539dc9bd303062b72328134fee23361
fpreston1 <42384725+fpreston1@users.noreply.github.com>
2018-09-06 15:50:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,22 +8,25 @@ const opusscript = require("opusscript");
 const xp = require("./xp.json");
 const fs = require("fs");
 
-// fs.readdir("./commands/", (err, files) => {
-// if(err) console.log(err);
-	
-// 	let jsfile = files.filter(f => f.split(".").pop() === "js")
-// 	if(jsfile.length <= 0){
-// 	console.log("Couldnt find commands.");
-// 	return;
-// 	}
-	
-// 	jsfile.forEach((f, i) => {
-// 		let props = require(`./commands/${f}`);
-// 		console.log(`${f} loaded!`);
-		
-// 		bot.commands.set(props.help.name, props);
-// 	});
-// });
+
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
 
 bot.on("ready", async () => {
 	console.log(`${bot.user.username} is online!`);
