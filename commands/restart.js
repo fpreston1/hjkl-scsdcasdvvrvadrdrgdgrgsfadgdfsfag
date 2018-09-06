46d017d88093f98b5d15f7524e0aae34a20909f0
fpreston1 <42384725+fpreston1@users.noreply.github.com>
2018-09-05 21:33:59 -0400

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.permissions.has("ADMINISTRATOR")) return;
-  
+ if(!message.member.roles.find(r => r.name === "Scrim Staff")) return;
+	
   message.channel.bulkDelete(10);
 	message.channel.send("Matches will now restart!").then(msg => msg.delete(10000));
 
